d477ee05218d24429b80c3247c4c42acc5d0f32d
Ruslan Shaydulin <rshaydu@node0738.palmetto.clemson.edu>
2016-11-26 19:32:46 -0500

    added todo plan

--- createdbtimeline.py
+++ createdbtimeline.py
@@ -1,3 +1,15 @@
+'''
+
+What should be done:
+
+1. Aggregate by year
+
+2. All internationals -- to dummy node
+
+3. Remove nonsense (maybe?..)
+
+'''
+
 import sqlite3
 import networkx as nx
 
@@ -10,6 +22,17 @@ c = conn.cursor()
 c.execute('CREATE TABLE airports (id REAL PRIMARY KEY NOT NULL, market_id REAL NOT NULL, in_degree_people REAL, out_degree_people REAL, in_degree_fare REAL, out_degree_fare REAL, year INT);')
 
 conn.commit()
+
+# like iterate over all files for a year (for all years, ofc)
+
+# then like for each such file add like nodes and whatever to the big graph (the one for the entire year)
+
+# offload to file or whatever??
+
+# oh and add lookup tables
+
+# seems about right 
+
 
 for node,data in G.nodes_iter(data=True):
 	in_peop = G.in_degree(node, weight='num_of_people')
