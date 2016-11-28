b443d72dfdc8f7290d1a390027393cdbe9a6d06e
Ruslan Shaydulin <rshaydu@node1195.palmetto.clemson.edu>
2016-11-28 10:36:26 -0500

    added a couple of csvs to plot for poster

--- readdbexample.py
+++ readdbexample.py
@@ -26,46 +26,26 @@ c = conn.cursor()
 
 #print(list(map(lambda x: x[0], c.description)))
 
-all_ids = []
+c.execute('SELECT fl_id FROM lookup WHERE fl_name=?', ('Atlanta',))
 
-for m_id in c.execute('SELECT fl_id FROM lookup'):
-	all_ids.append(m_id[0]) 
+air_id = (c.fetchone())[0]
+
 
 # for all markets
 
-for m_id in all_ids:
-	traffic = {}
-	traffic_legacy = []
-	employ = []
-	for year in range(1994,2015):
-		total_in = 0
-		total_out = 0
-		for q in range(1,4):
-			rows_fl = c.execute('SELECT in_degree_people, out_degree_people FROM airports WHERE year=? AND market_id=? AND quarter=?', (year,m_id, q))
-			for pair in rows_fl:
-				total_in += pair[0]
-				total_out += pair[1]
-			rows_ec = c.execute('SELECT mean_value FROM econ_data WHERE year=? AND MSA_id_airline=? AND quarter=?', (year, int(m_id), q))
-			#sum for all quarters
-			total_ec = 0
-			for value in rows_ec:
-				total_ec += value[0]
-			employ.append(total_ec/4)
-		traffic[total_in+total_out] = year 
-		traffic_legacy.append(total_in+total_out)
-	pr_tr = 1000000000 #previous traffic (i.e. in previous quarter)
-	prpr_tr = pr_tr
-	for q_tr in traffic_legacy:
-		if (float(q_tr)/float(pr_tr)) < 0.95 and (float(pr_tr)/float(prpr_tr)) < 0.95 and sum(traffic_legacy) > 5000000.0:
-			names = c.execute('SELECT fl_name, fl_state FROM lookup WHERE fl_id=?', (m_id,))
-			mar_name = ""
-			for n in names:
-				mar_name = (n[0] + ", " + n[1])
-			print("There was a depression in ", mar_name, "in", traffic[q_tr])
-		if q_tr == 0:
-			continue
-		prpr_tr = pr_tr
-		pr_tr = q_tr
-
+for year in range(1994,2016):
+	total_out = 0
+	total_in = 0
+	for q in range(1,5):
+		rows_fl = c.execute('SELECT in_degree_people, out_degree_people FROM airports WHERE year=? AND market_id=? AND quarter=?', (year,air_id, q))
+		for pair in rows_fl:
+			total_in += pair[0]
+			total_out += pair[1]
+		rows_ec = c.execute('SELECT mean_value FROM econ_data WHERE year=? AND MSA_id_airline=? AND quarter=?', (year, int(air_id), q))
+		total_ec = 0
+		for value in rows_ec:
+			total_ec += value[0]
+	out = str(year)+","+str(total_in+total_out)+","+str(total_ec/4)
+	print(out)
 
 conn.close()
